5f235c8d6d9eba5d29fffd7dcab867a010c799e0
Theodore Hadges <theodore.hadges@nyu.edu>
2019-12-08 23:45:05 -0500

    Add some visualizations

--- task2/src/scripts/similarity.py
+++ task2/src/scripts/similarity.py
@@ -10,6 +10,9 @@ from nltk.util import ngrams
 from collections import Counter
 #from similarity import *
 
+# set ngrams here
+NGRAMS = 3
+
 
 # note: cosine similarity function is from:
 # https://gist.github.com/gaulinmp/da5825de975ed0ea6a24186434c24fe4
@@ -36,16 +39,16 @@ def compute_jaccard_similarity(s1, s2):
     return(numerator/denominator)
 
 def write_to_json(cluster_id_dic):
-    with open('../../intermediary_data/filename_clusters.json', 'w') as fp:
+    with open('../../resources/filename_clusters.json', 'w') as fp:
         json.dump(cluster_id_dic, fp, sort_keys=True, indent=4)
 
 def write_list_to_txt(file_list):
-    with open('../../intermediary_data/filelist_axis_sim_matrix', 'w') as f:
+    with open('../../resources/filelist_axis_sim_matrix', 'w') as f:
         wr = csv.writer(f, quoting=csv.QUOTE_ALL)
         wr.writerow(file_list)
 
 def write_matrix_to_csv(M):
-    np.savetxt('../../intermediary_data/filename_similarity_matrix.csv', M, delimiter=',')
+    np.savetxt('../../resources/filename_similarity_matrix.csv', M, delimiter=',')
 
 def clean_string(s):
     s = s.lower()
@@ -55,7 +58,7 @@ def clean_string(s):
 def get_ngram_list_from_string(s):
     clean_s = clean_string(s)
     tokens = [token for token in s]
-    output = list(ngrams(tokens, 3)) 
+    output = list(ngrams(tokens, NGRAMS)) 
     return(output)
 
 if __name__ == "__main__":
@@ -67,9 +70,12 @@ if __name__ == "__main__":
 
     raw_list = raw_list = [re.sub("\[|\]|\'|\'|" "", "", item)for item in raw_list]
     raw_list = [re.sub(" " "", "", item)for item in raw_list] 
+    
     raw_list = list(set(raw_list)) # remove duplicate filenames 
    
     clean_list = [x.split(".")[1] for x in raw_list] 
+    clean_list = [re.sub("_", "", item)for item in raw_list] 
+
 
     # Use this small sample set for sanity check
     #test_list = ["hello", "hi", "cool", "school"]
@@ -83,14 +89,12 @@ if __name__ == "__main__":
             a = get_ngram_list_from_string(name_x)
             b = get_ngram_list_from_string(name_y)
 
-            #print(a, b)
             #M[i][j] = compute_jaccard_similarity(a, b)
-            #print(M[i][j])
             M[i][j] = cosine_similarity_ngrams(a, b)
 
 
     linkage_matrix = scipy.cluster.hierarchy.linkage(M)
-    fcluster = fcluster(linkage_matrix, t=0.8)
+    fcluster = fcluster(linkage_matrix, t=0.9)
 
     cluster_id_dic = {str(x): [] for x in fcluster}
 
@@ -106,8 +110,8 @@ if __name__ == "__main__":
     write_to_json(cluster_id_dic)
 
     # write similarity matrix to csv
-    #write_matrix_to_csv(M):
+    write_matrix_to_csv(M)
 
     # write file list to text file (the list represents the axes of the sim matrix)
-    #write_list_to_txt(clean_list):
+    write_list_to_txt(clean_list)
 
